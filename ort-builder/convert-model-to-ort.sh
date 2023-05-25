2f6aae5cb76bb448f680274fb46c17d09860f53f
motorik.michael <michael.gh.witte@cmapus.tu-berlin.de>
2023-05-25 12:49:43 +0200

    Updated ort-builder

--- ort-builder/convert-model-to-ort.sh
+++ ort-builder/convert-model-to-ort.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
 model_name=$1
 
 python -m onnxruntime.tools.convert_onnx_models_to_ort $1 --enable_type_reduction
 
-#rm -R ./model/
-#mkdir -p ./model
-
-#python -m bin2c -o ./model/$1.ort $1.ort
